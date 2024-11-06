9913b206b6523b5a0e40ca96e1a1a8db806bd2d4
Agustin Sanchez <agus.sanchez.240@gmail.com>
2024-11-06 19:15:43 -0300

    Change sign-in name validations and format

--- src/module/auth/controller/validations.js
+++ src/module/auth/controller/validations.js
@@ -1,17 +1,29 @@
 function validateSignUp(validationService) {
   return [
-    validationService.body('nombre').exists().withMessage('El nombre es requerido'),
-    validationService.body('apellido').exists().withMessage('El apellido es requerido'),
-    validationService.body('correoElectronico').isEmail().withMessage('El correo electrónico debe ser un email válido'),
-    validationService.body('contrasenia').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
-    validationService.body('idUsuarioTipo').optional().isEmpty().withMessage('No se puede enviar la propiedad idUsuarioTipo')
+    validationService.body('nombre').exists()
+      .withMessage('La propiedad nombre es requerida'),
+
+    validationService.body('apellido').exists()
+      .withMessage('La propiedad apellido es requerida'),
+
+    validationService.body('correoElectronico').isEmail()
+      .withMessage('La propiedad correoElectronico debe ser un email válido'),
+
+    validationService.body('contrasenia').isLength({ min: 6 })
+      .withMessage('La propiedad contrasenia debe tener al menos 6 caracteres'),
+
+    validationService.body('idUsuarioTipo').optional().isEmpty()
+      .withMessage('No se puede enviar la propiedad idUsuarioTipo')
   ];
 }
 
 function validateSignIn(validationService) {
   return [
-    validationService.body('email').isEmail().withMessage('email must be a valid email address'),
-    validationService.body('password').exists().withMessage('password is required')
+    validationService.body('correoElectronico').exists()
+      .withMessage('La propiedad correoElectronico es requerida para inciar sesión'),
+
+    validationService.body('contrasenia').exists()
+      .withMessage('La propiedad contrasenia es requerida para inciar sesión'),
   ];
 }
 
