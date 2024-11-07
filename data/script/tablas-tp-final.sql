91f0b72edab2738666d1accf4b2f59049b1ccd3a
Agustin Sanchez <agus.sanchez.240@gmail.com>
2024-11-07 08:45:17 -0300

    Update table usuarios seed in the sql script

--- data/script/tablas-tp-final.sql
+++ data/script/tablas-tp-final.sql
@@ -158,16 +158,13 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`idUsuario`, `nombre`, `apellido`, `correoElectronico`, `contrasenia`, `idUsuarioTipo`, `imagen`, `activo`) VALUES
-(1, 'Daenerys', 'Targaryen', 'daetar@correo.com', 'b2803ace294160fd87aa85f826fa8df0c39e77282e0217af680198cef8d9edc3', 1, NULL, 1),
-(2, 'Jon', 'Snow', 'jonsno@gmail.com', 'd98e05719dd7fa45547fbc3409eb36881bb8afe963268f7e8f6c2e24e80e58f5', 1, NULL, 1),
-(3, 'Tyrion', 'Lannister', 'tyrlan@correo.com', '9f9e51def43bc759ac35cd56ce8514a2c4dd0fbc9bfbb5bc97ce691f65bf5bb9', 2, NULL, 1),
-(4, 'Margaery', 'Tyrell', 'martyr@correo.com', 'ad872b4820b164b7a25695ff77d0f6e5df812c6f9944d1d21461f57f099bce57', 2, NULL, 1),
-(5, 'Samwell', 'Tarly', 'samtar@correo.com', 'a8487f98ab106b0ed2129a5446610b5ccba6b4bf7a937ef5194ce2f2a4c11bde', 2, NULL, 1),
-(6, 'Jeor', 'Mormont', 'jeomor@correo.com', 'ef0b04a6eba2d3cde7b32f53b2c13b509d198189cb9da2080c7259948cbc63ca', 2, NULL, 1),
-(7, 'Khal', 'Drogo', 'khadro@gmail.com', '84507cc9012d1c900abb65663e3b62633cb14073aa6569b60efa2b75cf431b37', 3, NULL, 1),
-(8, 'Catelyn', 'Stark', 'catsta@correo.com', '229e7f7177d0e221f889eb8d3e2b422eae42adc403412fb25718b84fe5fff4d7', 3, NULL, 1),
-(9, 'Yara', 'Greyjoy', 'yargre@correo.com', '097c61d6a3ee77e4f4a9d2c6b6fb284ee927a0c315f30172f685e4659a4f621b', 3, NULL, 1),
-(12, 'Jose', 'Battaglia', 'josbat@gmail.com', 'c30d798692466db470eafebfb04c272b359c80f2ebbac6f51f6e9ff9b6e3177b', 3, NULL, 1);
+(1, 'Agustin', 'Sanchez', 'agus@gmail.com', '$2b$10$7HtSXlZz6PA3Ls.prypvu.XxHze2K4wkyVEBGio2zOA7WC4HHtDDa', 1, NULL, 1),
+(2, 'Lara', 'Alegre', 'lara@gmail.com', '$2b$10$rZeI4w236ysneDwwhEFL8uRRQ2fGpOFTR3Ah0U3u2bNuxpEar7vp6', 1, NULL, 1),
+(3, 'Lucas', 'Ruiz', 'lucas@gmail.com', '$2b$10$8nFCGXQMjjOoJ2QGXCDpA.MeRWYzHiPQRbDFuoMZtVqY1.D6KR.NG', 2, NULL, 1),
+(4, 'Julian', 'Annoni', 'julian@gmail.com', '$2b$10$Dy04i3WU1T.eg/EHBPfjE./aaaX.xpL5qJYEQq.a.uCUxaXansqNq', 2, NULL, 1),
+(5, 'Giovanni', 'Percara', 'gio@gmail.com', '$2b$10$rQf8ptHHdAxrvdKBfbdLN.K80G6l2tvR6tITEVM/foCfiDOm7TmPm', 3, NULL, 1),
+(6, 'Daniela', 'Sanchez', 'daniela@gmail.com', '$2b$10$ThoYGvlZJI/jGmhocCr3a.zS0nAevlmiKtbTLavz69N5Q/O6fqLX6', 3, NULL, 1);
+(7, 'Pipo', 'Gorosito', 'pipo@gmail.com', '$2b$10$qfCGIE4ClfGj6iuRgo8eFeU8igAOnSeQR09u1aTdF.KiIESkDOT16', 3, NULL, 1);
 
 -- --------------------------------------------------------
 
