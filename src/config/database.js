2078f4438ead616c2638f3b9f34013699ddd45f3
Agustin Sanchez <agus.sanchez.240@gmail.com>
2024-11-07 21:30:07 -0300

    Add environment variables in the database config file

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,14 @@
 import { createPool } from 'mysql2/promise';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const databaseConnection = createPool({
-  host: process.env.DB_HOST || "localhost",
-  port:process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD ||"",
-  database: process.env.DB_NAME || "database",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
